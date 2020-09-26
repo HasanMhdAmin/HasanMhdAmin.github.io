1d7198331bcb0d60b62a308aaae0ab30337ffbab
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2020-09-27 00:43:30 +0200

    Updates

---
+++ precache-manifest.0f50f06c647a22663ffa9ce1e156a8a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e0ad8121038db4773bd0b719d18461",
+    "url": "/hasan-cv/index.html"
+  },
+  {
+    "revision": "8a129e708e33ddfbedee",
+    "url": "/hasan-cv/static/css/main.56d3355c.chunk.css"
+  },
+  {
+    "revision": "ff1deb1d469a97715208",
+    "url": "/hasan-cv/static/js/2.0a208dfd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hasan-cv/static/js/2.0a208dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a129e708e33ddfbedee",
+    "url": "/hasan-cv/static/js/main.23c6bc96.chunk.js"
+  },
+  {
+    "revision": "371457efa135c0c1df9c",
+    "url": "/hasan-cv/static/js/runtime-main.d77f5452.js"
+  }
+]);
