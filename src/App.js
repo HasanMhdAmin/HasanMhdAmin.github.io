5087048f55e0670c56ff2d4012ab5a87fdae39f2
Hasan Mhd Amin <hasan_amin@deichmann.com>
2024-01-07 17:46:22 +0100

    Add PrivacyPolicy.html

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     function hashLinkScroll() {
         const { hash } = window.location;
         if (hash !== '') {
-            // Push onto callback queue so it runs after the DOM is updated,
+            // Push onto callback queue, so it runs after the DOM is updated,
             // this is required when navigating from a different page so that
             // the element is rendered on the page before trying to getElementById.
             setTimeout(() => {
